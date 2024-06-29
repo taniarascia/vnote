c086efa6d8a0518e4301263970d27976ec46a5ae
Bhargav Parsi <bhargav265@gmail.com>
2024-06-29 15:57:02 -0700

    Update index.ts

--- src/client/api/index.ts
+++ src/client/api/index.ts
@@ -7,7 +7,7 @@ const scratchpadNote = {
   id: uuid(),
   text: `# Scratchpad
 
-The easiest note to find.`,
+The easiest note to find this.`,
   category: '',
   scratchpad: true,
   favorite: false,
